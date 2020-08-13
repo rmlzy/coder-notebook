933eb524d0b1082d0fb5935a47c352d08acf43f9
rmlzy <rmlzy@outlook.com>
2020-08-13 11:21:30 +0800

    feat: init repo with simulatedgreg/electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'coder-notebook'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
